f4ac3f8807d2cb79de3ea73bdc9f78f33b0ad0cc
Thejas <thejas@leuwint.com>
2025-05-05 12:02:12 +0530

    admin

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use('/api/v1/payment-requests', volunteerPaymentRouter);
 app.use('/api/v1/admin/payment-requests', adminPaymentRouter);
 app.use('/api/v1/admin/uploads', uploadRouter);
 
-app.use('/api/v1/pdf',letterheadPdfRoutes);
+app.use('/api/v1/admin/pdf',letterheadPdfRoutes);
 removeUnverifiedAccounts();
 connection();
 
